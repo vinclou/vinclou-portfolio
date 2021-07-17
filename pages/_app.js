f016a008f5090835c2e494260ef95f8c63accfc7
Vincent Arlou <arlou@protonmail.com>
2021-07-17 13:26:16 -0400

    👌 update: config files

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
+/* Fonts */
+import '@fontsource/sora';
+import '@fontsource/antic-didone';
+import '@fontsource/baloo-bhai-2';
+/* Fonts */
 import customTheme from '../styles/theme';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
