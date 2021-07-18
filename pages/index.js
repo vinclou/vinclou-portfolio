055f9d2029dc706b6ddf888333d9fa52ec96c520
Vincent Arlou <arlou@protonmail.com>
2021-07-18 14:51:21 -0400

    ⚗️ Experiments with SVG and framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,81 @@
 import Head from 'next/head';
 import '@fontsource/sora/400.css';
 import '@fontsource/sora/700.css';
-import { Box, Flex, Heading, Link, Text, Icon } from '@chakra-ui/react';
-import { useColorMode } from '@chakra-ui/color-mode';
-import { Button } from '@chakra-ui/button';
-import { Center } from '@chakra-ui/layout';
-import { VStack } from '@chakra-ui/layout';
-import { Logo } from '../components/assets/logo';
-import { Construction } from '../components/assets/construction';
+import { Container } from '@/layouts/container';
+import { useScrollPosition } from '@/hooks/useScrollPosition';
+import { ContentWrapper } from '@/layouts/contentWrapper';
+import { useMediaQuery } from '@chakra-ui/media-query';
+import { Box, Flex, Icon, VStack } from '@chakra-ui/react';
+import { HeroVisual } from '@/components/svg';
+import { MyTestSvg } from '@/components/svg/myTestSvg';
+import { useColorModeSwitcher } from '@/utils/hooks/useColorModeSwitcher';
+import { BsArrowDown } from 'react-icons/bs';
 
+//TODO make all the needed components
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Flex minH="100vh" direction="column">
-      <Head>
-        <title>Vincent Arlou</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Flex p="2rem" justify="space-between" align="center">
-        <Logo />
-        <Button onClick={toggleColorMode}>
-          Lights
-          {colorMode === 'light' ? ' off' : ' on'}
-        </Button>
-      </Flex>
-
-      <Center mt="20vh">
-        <VStack textAlign="center" spacing="4">
-          <Construction boxSize="7rem" />
-          <Heading as="h1" variant="h1">
-            This site is currently under construction
-          </Heading>
-          <Heading variant="h2">
-            Connect with me on{' '}
-            <Link
-              color={colorMode === 'light' ? 'purple.600' : 'purple.300'}
-              isExternal
-              href="https://github.com/vinclou"
-            >
-              GitHub
-            </Link>{' '}
-            or{' '}
-            <Link
-              color={colorMode === 'light' ? 'red.600' : 'red.300'}
-              isExternal
-              href="https://www.linkedin.com/in/vincent-arlou-933a44163/"
-            >
-              LinkedIn
-            </Link>
-          </Heading>
-        </VStack>
-      </Center>
-    </Flex>
+    <Container>
+      <ContentWrapper>
+        <Hero />
+        {/* <FeatureProjects />
+        <Subscribe /> */}
+      </ContentWrapper>
+    </Container>
   );
 }
+
+const Hero = () => {
+  const [isXtraLarge] = useMediaQuery('(min-width: 1280px)');
+  const { scrollPos } = useScrollPosition();
+
+  return (
+    <Box w={{ base: '90%', '2xl': '100%' }}>
+      <Flex mb="4rem" justify="space-between">
+        <Box w={{ base: '100%', xl: '50%' }}>
+          Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
+          Hello Hello Hello Hello
+        </Box>
+        {/* {isXtraLarge && <HeroVisual />} */}
+        {isXtraLarge && <MyTestSvg />}
+      </Flex>
+      {isXtraLarge && <ScrollArrow scrollPos={scrollPos} />}
+    </Box>
+  );
+};
+
+const ScrollArrow = ({ scrollPos }) => {
+  const { colorDark } = useColorModeSwitcher();
+  return (
+    <VStack
+      opacity={scrollPos ? 0 : 1}
+      transition="visibility 0s linear 300ms, opacity 600ms"
+      // might as well put display none
+      visibility={scrollPos ? 'hidden' : 'visible'}
+      // display={scrollPos ? 'none' : 'inline'}
+    >
+      <span>{`<Scroll>`}</span>
+      <Icon
+        as={BsArrowDown}
+        boxSize="1.5em"
+        fill={colorDark}
+        css={`
+          animation: arrowAnimation 1.5s infinite ease-in-out;
+          @keyframes arrowAnimation {
+            0% {
+              opacity: 0;
+            }
+            30% {
+              opacity: 1;
+            }
+            60% {
+              opacity: 1;
+            }
+            to {
+              opacity: 0;
+            }
+          }
+        `}
+      />
+    </VStack>
+  );
+};
---
+++ utils/hooks/useScrollPosition.js
@@ -0,0 +1,35 @@
+/*
+	Custom Hook which can be used to determine if element was scrolled or not
+*/
+import { useState, useEffect } from 'react';
+
+function useScrollPosition() {
+  const [scrollPos, setScrollPos] = useState();
+
+  const listenScroll = () => {
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolledFloat = windowScroll / height;
+
+    const scrolled = scrolledFloat > 0.05;
+
+    setScrollPos(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenScroll);
+
+    return () => {
+      window.removeEventListener('scroll', listenScroll);
+    };
+  });
+
+  return { scrollPos };
+}
+
+export { useScrollPosition };
