3536e623248b0a679849ee215309f8bd45f3ec57
Vincent Arlou <arlou@protonmail.com>
2021-07-19 11:58:32 -0400

    set general component composition for the about page ✨

---
+++ pages/about.js
@@ -0,0 +1,46 @@
+import React from 'react';
+// chakra
+import { Box, HStack } from '@chakra-ui/layout';
+import { Icon } from '@chakra-ui/icon';
+// wrappers
+import { Container } from '@/layout/container';
+import { ContentWrapper } from '@/layouts/contentWrapper';
+// hooks
+import { useToggle } from '@/utils/hooks/useToggle';
+import { useColorModeSwitcher } from '@/utils/hooks/useColorModeSwitcher';
+// data
+import { github, linkedin } from '@/data/socials';
+
+export default function About() {
+  return (
+    <Container title="About | Vincent Arlou">
+      <ContentWrapper>
+        <Intro />
+        <Skills />
+        <Contact />
+      </ContentWrapper>
+    </Container>
+  );
+}
+
+/* MODULES TODO: seperate them */
+const Intro = () => {
+  return <Box>Intro</Box>;
+};
+
+const Skills = () => {
+  return <Box>Skills</Box>;
+};
+
+const Contact = () => {
+  return <Box>Contact Me</Box>;
+};
+
+const SectionHeading = ({ children, ...props }) => {
+  const { colorGrey } = useColorModeSwitcher();
+  return (
+    <HStack {...props} w="100%">
+      <Box flex="1" h="1.5px" bg={colorGrey} />
+    </HStack>
+  );
+};
