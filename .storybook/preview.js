16d835baf0cdef2a1ffc1cdd709df211eef45063
Vincent Arlou <arlou@protonmail.com>
2021-08-11 10:55:13 +0200

    🎨 IMPROVE: Chakra/Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,21 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import customTheme from '../styles/theme';
+import React from 'react';
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider theme={customTheme}>
+      <Story />
+    </ChakraProvider>
+  )
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
