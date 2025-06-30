c386d0b56ca17f075f976440de2644ef859e0329
Vincent Arlou <54914693+vinclou@users.noreply.github.com>
2025-06-30 13:45:33 -0500

    Fix MDX Prism compatibility with Next.js 14

--- utils/mdx.js
+++ utils/mdx.js
@@ -3,7 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import { serialize } from 'next-mdx-remote/serialize';
 import readingTime from 'reading-time';
-import mdxPrism from 'mdx-prism';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 const ROOT = path.join(process.cwd(), 'data');
 
@@ -34,9 +34,29 @@ export async function getFileBySlug(type, slug) {
         ],
         require('remark-code-titles')
       ],
-      rehypePlugins: [mdxPrism]
+      rehypePlugins: [
+        [
+          rehypePrettyCode,
+          {
+            theme: 'github-dark',
+            keepBackground: false,
+            onVisitLine(node) {
+              // Prevent lines from collapsing in `display: grid` mode, and allow empty
+              // lines to be copy/pasted
+              if (node.children.length === 0) {
+                node.children = [{ type: 'text', value: ' ' }];
+              }
+            },
+            onVisitHighlightedLine(node) {
+              node.properties.className.push('line--highlighted');
+            },
+            onVisitHighlightedWord(node) {
+              node.properties.className = ['word--highlighted'];
+            }
+          }
+        ]
+      ]
     }
-    // target: ['esnext']
   });
 
   return {
